82d57f14b9bf8eaad72d6ace0ce65bb1711acd54
awahab111 <118908180+awahab111@users.noreply.github.com>
2023-04-01 23:11:30 +0500

    Added player.h

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <ctime>
-#include <random>
+#include "Player.h"
 using namespace sf;
 using namespace std;
-int random_num(int upper, int lower);
-RectangleShape initialize_grid(int area);
-void draw_grid(int width, int area, RenderWindow &w ,RectangleShape &rect);
-Sprite create_player(string filename, int gridlen, int area);
-void PlayerInput_1(Sprite& player, Event &e ,int size);
 
 
 int main()
@@ -19,16 +13,22 @@ int main()
     int gridWidth = random;
     int gridHeight = random;
     int rectSize = 65 ;
-
     RectangleShape rect = initialize_grid(rectSize);
 
+    //create window to display
     RenderWindow window(VideoMode((gridHeight*rectSize), gridWidth*rectSize), "SFML Grid");
 
-    Sprite ch_1 = create_player("Goujo.png", gridWidth, rectSize);
-    Sprite ch_2 = create_player("itadori.png",gridWidth, rectSize);
-    Sprite ch_3 = create_player("meg.png",gridWidth, rectSize);
-    Sprite item = create_player("finger.png",gridWidth,0.1);
+    //Players
+    Player characters[3] = { Player("resources/Goujo.png", gridWidth, rectSize),Player("resources/itadori.png",gridWidth, rectSize),Player("resources/meg.png",gridWidth, rectSize)};
 
+    //Items
+    Item * finger[20];
+    for (int i = 0; i < 20; i++){
+        finger[i] = new Item("resources/finger.png", gridWidth, rectSize);
+    }
+    
+
+    //Main loop
     while (window.isOpen()) {
         Event event;
         while (window.pollEvent(event)) {
@@ -36,86 +36,39 @@ int main()
 
             // Player Input
             if (event.type == Event::KeyPressed) {
-                PlayerInput_1(ch_1, event, rectSize);
+                characters[0].PlayerInput_1(event, rectSize);
+                characters[1].PlayerInput_2(event, rectSize);
+                characters[2].PlayerInput_3(event, rectSize);
+            }
         }
+
+        //Gathering items
+        bool gamefinish = true;
+        for (int j = 0; j < 3; j++)
+        {
+            for (int i = 0; i < 20; i++){
+                if (finger[i]->isColliding(characters[j].playerSprite)){
+                    finger[i]->setPosition(999,999);
+                    finger[i]->setActive(false);
+                }
+                if (finger[i]->getActive() == true){gamefinish = false;}
+            }
+            if (gamefinish){ cout << "Game Finished" << endl; return 0;}
+            
         }
+
+        //Updating window       
+
         window.clear(Color::Black);
 
         draw_grid(gridWidth, rectSize, window, rect);
-        window.draw(ch_1);
-        window.draw(ch_2);
-        window.draw(ch_3);
-        window.draw(item);
 
-
+        for (int i = 0; i < 3; i++){window.draw(characters[i].playerSprite);}
+        for (int i = 0; i < 20; i++){window.draw(finger[i]->itemSprite);}
+        
         window.display();
     }
+ 
     
-    return 0;
-}
-
-int random_num(int upper, int lower){
-    random_device seed;
-    mt19937 engine(seed());
-    uniform_int_distribution<int> dist(1, 14);
-    return dist(engine);
-}
-
-RectangleShape initialize_grid(int area){
-    RectangleShape rect(Vector2f(area, area));
-    rect.setFillColor(Color::Green);
-    rect.setOutlineThickness(2);
-    rect.setOutlineColor(Color::White);
-    return rect;
-}
-
-
-void draw_grid(int width, int area, RenderWindow &w , RectangleShape &rect){
-    // draw the grid
-    for (int y = 0; y < width; y++) {
-        for (int x = 0; x < width; x++) {
-            rect.setPosition(Vector2f(x * area, y * area));
-            w.draw(rect);
-        }
-    }
-    
-}
-
-void PlayerInput_1(Sprite& player, Event &e , int size){
-    if (e.key.code == Keyboard::Left && player.getPosition().x > size) {
-        // Move player left
-        player.move(-1*size, 0.f);
-    } else if (e.key.code == Keyboard::Right && player.getPosition().x < size * 14 ) {
-        // Move player right
-        player.move(size, 0.f);
-    } else if (e.key.code == Keyboard::Up  && player.getPosition().y > size) {
-        // Move player left
-        player.move(0.f, -1*size);
-    } else if (e.key.code == Keyboard::Numpad2  && player.getPosition().y <size * 14) {
-        // Move player right
-        player.move(0.f, size);
-    }
-}
-
-Sprite create_player(string filename, int gridlen, int area){
-
-    // generate a random number between 1-14
-    int random_width = random_num(14,1);
-    int random_height = random_num(14,1);
-
-    // Taking texture for the player 
-    Texture * playerTexture = new Texture;
-    if (!playerTexture->loadFromFile(filename)) {
-        cout << "Error Loading "<< filename << endl;
-    }
-    playerTexture->setSmooth(true);
-
-    // Applying texture
-    Sprite playerSprite;
-    playerSprite.setTexture(*playerTexture);
-    playerSprite.setPosition(20.f, 1.f);
-    playerSprite.move(random_width * area, random_height * area);
-    playerSprite.scale(0.34f, 0.34f);
-    return playerSprite;
-
-}
+} 
+         
---
+++ player.h
@@ -0,0 +1,199 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <random>
+using namespace std;
+using namespace sf;
+
+int random_num(int upper, int lower);
+RectangleShape initialize_grid(int area);
+void draw_grid(int width, int area, RenderWindow &w ,RectangleShape &rect);
+
+
+class Player {
+private:
+    Texture playerTexture;
+    float positionX;
+    float positionY;
+    int points;
+public:
+    FloatRect boundingbox;
+    Sprite playerSprite;
+    
+    Player(std::string filename, int gridlen, int area);
+    void PlayerInput_1(Event &e , int size);
+    void PlayerInput_2(Event &e , int size);
+    void PlayerInput_3(Event &e , int size);
+
+    void update_position();
+
+};
+
+Player::Player(std::string filename, int gridlen, int area) {
+    int random_width = random_num(gridlen - 1, 1);
+    int random_height = random_num(gridlen -1, 1);
+
+    // Taking texture for the player
+    if (!playerTexture.loadFromFile(filename)) {
+        std::cout << "Error Loading " << filename << std::endl;
+    }
+    playerTexture.setSmooth(true);
+
+    // Applying texture
+    playerSprite.setTexture(playerTexture);
+    playerSprite.setPosition(20.f, 1.f);
+    playerSprite.move(random_width * area, random_height * area);
+    playerSprite.scale(0.32f, 0.32f);
+
+    //Getting player position
+    positionX = playerSprite.getPosition().x;
+    positionX = playerSprite.getPosition().y;
+
+    //Creating a bounding box
+    boundingbox = playerSprite.getGlobalBounds();
+}
+
+void Player::PlayerInput_1(Event &e , int size) {
+    if (e.key.code == Keyboard::Numpad4 && playerSprite.getPosition().x > size) {
+        // Move playerSprite left
+        playerSprite.move(-1*size, 0.f);
+    } 
+    else if (e.key.code == Keyboard::Numpad6 && playerSprite.getPosition().x < size * 14 ) {
+        // Move playerSprite right
+        playerSprite.move(size, 0.f);
+    } 
+    else if (e.key.code == Keyboard::Numpad8  && playerSprite.getPosition().y > size) {
+        // Move playerSprite up
+        playerSprite.move(0.f, -1*size);
+    } 
+    else if (e.key.code == Keyboard::Numpad2  && playerSprite.getPosition().y <size * 14) {
+        // Move playerSprite down
+        playerSprite.move(0.f, size);
+    }
+    update_position();
+}
+
+void Player::PlayerInput_2(Event &e , int size) {
+    if (e.key.code == Keyboard::A && playerSprite.getPosition().x > size) {
+        // Move playerSprite left
+        playerSprite.move(-1*size, 0.f);
+    } 
+    else if (e.key.code == Keyboard::D && playerSprite.getPosition().x < size * 14 ) {
+        // Move playerSprite right
+        playerSprite.move(size, 0.f);
+    } 
+    else if (e.key.code == Keyboard::W  && playerSprite.getPosition().y > size) {
+        // Move playerSprite up
+        playerSprite.move(0.f, -1*size);
+    } 
+    else if (e.key.code == Keyboard::S  && playerSprite.getPosition().y <size * 14) {
+        // Move playerSprite down
+        playerSprite.move(0.f, size);
+    }
+    update_position();
+}
+
+void Player::PlayerInput_3(Event &e , int size) {
+    if (e.key.code == Keyboard::H && playerSprite.getPosition().x > size) {
+        // Move playerSprite left
+        playerSprite.move(-1*size, 0.f);
+    } 
+    else if (e.key.code == Keyboard::K && playerSprite.getPosition().x < size * 14 ) {
+        // Move playerSprite right
+        playerSprite.move(size, 0.f);
+    } 
+    else if (e.key.code == Keyboard::U  && playerSprite.getPosition().y > size) {
+        // Move playerSprite up
+        playerSprite.move(0.f, -1*size);
+    } 
+    else if (e.key.code == Keyboard::J  && playerSprite.getPosition().y <size * 14) {
+        // Move playerSprite down
+        playerSprite.move(0.f, size);
+    }
+    update_position();
+}
+
+void Player::update_position() {
+    positionX = playerSprite.getPosition().x;
+    positionX = playerSprite.getPosition().y;
+}
+
+class Item {
+private:
+    Texture itemtexture;
+    FloatRect boundingBox;
+    bool isActive;
+
+public:
+    Sprite itemSprite;
+    Item(){}
+    Item(std::string filename,int gridlen, int area) {
+        //Load Texture
+        if (!itemtexture.loadFromFile(filename)) {
+            std::cout << "Error Loading " << filename << endl;
+        }
+
+        int random_width = random_num(gridlen - 1, 1);
+        int random_height = random_num(gridlen -1, 1);
+
+
+        itemSprite.setTexture(itemtexture);
+        itemSprite.setPosition(05.f, -2.f);
+        itemSprite.move(random_width * area, random_height * area);
+        itemSprite.scale(0.08f, 0.08f);
+        isActive = true;
+        boundingBox = itemSprite.getGlobalBounds();
+    }
+
+    bool isColliding(Sprite otherBox) {
+        return boundingBox.contains(otherBox.getPosition());
+    }
+
+    void setActive(bool active) {
+        isActive = active;
+    }
+
+    bool getActive() {
+        return isActive;
+    }
+
+    void setPosition(float x, float y) {
+        itemSprite.setPosition(x, y);
+    }
+
+    FloatRect getBoundingBox() {
+        return boundingBox;
+    }
+};
+
+
+
+
+int random_num(int upper, int lower){
+    random_device seed;
+    mt19937 engine(seed());
+    uniform_int_distribution<int> dist(1, 14);
+    return dist(engine);
+}
+
+RectangleShape initialize_grid(int area){
+    RectangleShape rect(Vector2f(area, area));
+    rect.setFillColor(Color::Green);
+    rect.setOutlineThickness(2);
+    rect.setOutlineColor(Color::White);
+    return rect;
+}
+
+
+void draw_grid(int width, int area, RenderWindow &w , RectangleShape &rect){
+    // draw the grid
+    for (int y = 0; y < width; y++) {
+        for (int x = 0; x < width; x++) {
+            rect.setPosition(Vector2f(x * area, y * area));
+            w.draw(rect);
+        }
+    }
+    
+}
+
+
+
